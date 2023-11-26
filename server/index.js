f2f5a5fb49cf771834b39fc28f264a374f0d130d
Abdulls2112 <152040360+Abdulls2112@users.noreply.github.com>
2023-11-26 08:02:50 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(authenticateToken);
+
 
 const db = mysql.createConnection({
   host: 'database-1.caoacq3ev5m0.eu-north-1.rds.amazonaws.com',

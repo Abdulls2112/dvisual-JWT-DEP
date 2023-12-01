7a47080161de4b6e0b1750c5a96ac1e917dd2a8c
Abdulls2112 <152040360+Abdulls2112@users.noreply.github.com>
2023-11-30 16:37:38 -0800

    Update Profile.jsx

--- client/src/component/Profile.jsx
+++ client/src/component/Profile.jsx
@@ -58,6 +58,50 @@ const Profile = () => {
     fetchData();
   }, [history]);
 
+  const handleAddSite = async () => {
+  try {
+    // Fetch the token from local storage
+    const token = localStorage.getItem('token');
+
+    // Check if the token is present
+    if (!token) {
+      // Handle the case where the token is not present
+      console.error('Token not found in local storage');
+      history.push('/login');
+      return;
+    }
+
+    // Send the token to the backend for authentication
+    const response = await axios.post(
+      'https://dvisual-deployment.vercel.app/add-site',
+      {
+        site_name: siteName,
+        site_location: siteLocation,
+      },
+      {
+        headers: {
+          authorization: `${token}`,
+        },
+        withCredentials: true,
+      }
+    );
+
+    if (response.data.success) {
+      setShowAddSiteForm(false);
+      setSiteName('');
+      setSiteLocation('');
+      setError('');
+
+    } else {
+      setError(response.data.error);
+    }
+  } catch (error) {
+    console.error('Error adding site:', error);
+    setError('Internal Server Error');
+  }
+};
+
+
   const handleSiteButtonClick = (siteId) => {
     history.push(`/visualize/${siteId}`);
   };
@@ -79,6 +123,34 @@ const Profile = () => {
             </div>
           ))}
         </div>
+         {showAddSiteForm ? (
+            <>
+            <div class="afterbutton">
+            <div>
+                <label class="sitename-label">Site Name:</label>
+                <input
+                  type="text"
+                  value={siteName}
+                  onChange={(e) => setSiteName(e.target.value)}
+                />
+              </div>
+              <div>
+                <label class="sitelocation-label">Site Location:</label>
+                <input
+                  type="text"
+                  value={siteLocation}
+                  onChange={(e) => setSiteLocation(e.target.value)}
+                />
+                <button  onClick={handleAddSite}>Add Site</button>
+                <button onClick={() => setShowAddSiteForm(false)}>Cancel</button>
+              </div>
+
+            </div>
+           
+            </>
+          ) : (
+            <button class="mysitebutton" onClick={() => setShowAddSiteForm(true)}>Add Site</button>
+          )}
         <div className="box">
           <p>Email: {login ? email : null}</p>
           <p>Organization Name: {organizationName}</p>

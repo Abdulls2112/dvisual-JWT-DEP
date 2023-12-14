25c6b8dc5a8b0e4a285406ac164dbf5a7979c767
Abdulls2112 <152040360+Abdulls2112@users.noreply.github.com>
2023-12-14 14:55:43 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,151 +1,327 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
+const crypto = require('crypto');
 
-const Profile = () => {
-  const [email, setEmail] = useState('');
-  const [organizationName, setOrganizationName] = useState('');
-  const [login, setLogin] = useState(false);
-  const [sites, setSites] = useState([]);
-  const history = useHistory();
-  const [error, setError] = useState('');
-  const [siteName, setSiteName] = useState('');
-  const [siteLocation, setSiteLocation] = useState('');
-  const [showAddSiteForm, setShowAddSiteForm] = useState(false);
+const PORT = process.env.PORT || 8000;
+const BaseURL = process.env.URL || '';
+const app = express();
+const secretKey = crypto.randomBytes(32).toString('hex');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Retrieve token from local storage
-        const token = localStorage.getItem('token');
 
-        if (!token) {
-          console.error('Token not found in local storage');
-          return history.push('/login');
+app.use(cors({
+  origin: 'https://dvisual-jwt-dep.vercel.app',
+  credentials: true,
+}));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const db = mysql.createConnection({
+  host: 'database-1.caoacq3ev5m0.eu-north-1.rds.amazonaws.com',
+  user: 'abdullah',
+  password: 'abdullah-w-21',
+  database: 'dvisual'
+});
+
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+app.post("/register", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const organisationname = req.body.organisationname;
+
+    // Generate unique ids using UUID
+    const organisation_id = uuidv4();
+    const role_id = uuidv4();
+
+    // Set a default value of 'True' for status
+    const status = true;
+
+    // Generate a random activation date between now and 30 days in the future
+    const currentDate = new Date();
+    const futureDate = new Date();
+    futureDate.setDate(currentDate.getDate() + 30);
+
+    // Generate a random date within the specified range
+    const randomDate = new Date(
+        currentDate.getTime() + Math.random() * (futureDate.getTime() - currentDate.getTime())
+    );
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err);
+            return res.send({ msg: "Error while hashing password" });
         }
 
-        const userResponse = await axios.get('https://dvisual-server-api.vercel.app/user', {
-          headers: {
-            authorization: `${token}`,
-          },
+        const data = {
+            email: email,
+            password: hash,
+            organisationname: organisationname,
+            organisation_id: organisation_id,
+            status: status,
+            activedate: randomDate,
+        };
+
+        let sqll = `SELECT * FROM users WHERE email='${email}'`;
+        db.query(sqll, (er, ress) => {
+            if (ress.length > 0) {
+                res.send({ msg: "User Email Already Present" });
+            } else {
+                // Insert into the organizations table
+                let organizationSql = "INSERT INTO organizations (organisation_id, organisationname) VALUES (?, ?)";
+                let organizationValues = [organisation_id, organisationname];
+
+                db.query(organizationSql, organizationValues, (orgErr, orgResult) => {
+                    if (orgErr) {
+                        console.log(orgErr);
+                        return res.send({ msg: "Error while adding organization" });
+                    }
+
+                    // Now that the organisation_id and organisationname are inserted, proceed to insert into the users table
+                    let sql = "INSERT INTO users SET ?";
+                    db.query(sql, data, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                            return res.send({ msg: "Error while registering user" });
+                        }
+
+                        // Now that the user is registered, you can get the user_id
+                        const user_id = result.insertId;
+
+                        // Set the user_id and organisation_id for the roleData
+                        const roleData = {
+                            role_id: role_id,
+                            user_id: user_id,
+                            role: "user",
+                        };
+
+                        db.query("INSERT INTO user_roles SET ?", roleData, (roleErr, roleResult) => {
+                            if (roleErr) {
+                                console.log(roleErr);
+                                return res.send({ msg: "Error while assigning role" });
+                            }
+
+                            return res.send({ msg: "User registered successfully" });
+                        });
+                    });
+                });
+            }
         });
+    });
+});
 
-        if (userResponse.data.success) {
-          setLogin(true);
-          setEmail(userResponse.data.user.email);
 
-          const organizationResponse = await axios.get(`https://dvisual-server-api.vercel.app/organization`);
-          setOrganizationName(organizationResponse.data.organizationname);
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-          const sitesResponse = await axios.get(`https://dvisual-server-api.vercel.app/sites`);
-          setSites(sitesResponse.data.sites);
-        } else {
-          history.push('/login');
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setError('Internal Server Error');
-      }
-    };
-
-    fetchData();
-  }, [history]);
-
-  const handleAddSite = async () => {
-    try {
-      const token = localStorage.getItem('token');
-
-      if (!token) {
-        console.error('Token not found in local storage');
-        return history.push('/login');
-      }
-
-      const response = await axios.post(
-        'https://dvisual-deployment.vercel.app/add-site',
-        {
-          site_name: siteName,
-          site_location: siteLocation,
-        },
-        {
-          headers: {
-            authorization: `${token}`,
-          },
-          withCredentials: true,
-        }
-      );
-
-      if (response.data.success) {
-        setShowAddSiteForm(false);
-        setSiteName('');
-        setSiteLocation('');
-        setError('');
-        // Fetch the updated list of sites and update the state accordingly
-        const updatedSitesResponse = await axios.get(`https://dvisual-server-api.vercel.app/sites`);
-        setSites(updatedSitesResponse.data.sites);
-      } else {
-        setError(response.data.error);
-      }
-    } catch (error) {
-      console.error('Error adding site:', error);
-      setError('Internal Server Error');
+  let sql = `SELECT * FROM users WHERE email='${email}'`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ msg: 'Error while retrieving user information' });
     }
-  };
+    if (result.length > 0) {
+      bcrypt.compare(password, result[0].password, (err, response) => {
+        if (response) {
+          const user = result[0];
+          const token = jwt.sign({ userId: user.id, email: user.email }, '0GJsp4gtlpqiSccUvZCh1XB1XtAOAJTITKj0BRtlQP8', { expiresIn: '8h' });
+          return res.json({ login: true, useremail: email, token });
+        } else {
+          return res.json({ login: false, msg: 'Wrong Password' });
+        }
+      });
+    } else {
+      return res.json({ login: false, msg: 'User Email Not Exists' });
+    }
+  });
+});
 
-  const handleSiteButtonClick = (siteId) => {
-    history.push(`/visualize/${siteId}`);
-  };
+// Update the '/organization' endpoint to use JWT for authentication
+app.get('/organization', authenticateToken, (req, res) => {
+  const userId = req.decoded.userId;
 
-  return (
-    <>
-      <section
-        style={{
-          backgroundColor: '#f8f8f8',
-          color: '#333',
-          width: '100%',
-          minHeight: '90vh',
-        }}
-      >
-        <div className="sitedabba container-xxl">
-          {sites.map((site) => (
-            <div class="added-button" key={site.site_id} onClick={() => handleSiteButtonClick(site.site_id)}>
-              {site.site_name} - {site.site_location}
-            </div>
-          ))}
-        </div>
-        {showAddSiteForm ? (
-          <>
-            <div class="afterbutton">
-              <div>
-                <label class="sitename-label">Site Name:</label>
-                <input
-                  type="text"
-                  value={siteName}
-                  onChange={(e) => setSiteName(e.target.value)}
-                />
-              </div>
-              <div>
-                <label class="sitelocation-label">Site Location:</label>
-                <input
-                  type="text"
-                  value={siteLocation}
-                  onChange={(e) => setSiteLocation(e.target.value)}
-                />
-                <button onClick={handleAddSite}>Add Site</button>
-                <button onClick={() => setShowAddSiteForm(false)}>Cancel</button>
-              </div>
-            </div>
-          </>
-        ) : (
-          <button class="mysitebutton" onClick={() => setShowAddSiteForm(true)}>Add Site</button>
-        )}
-        <div className="box">
-          <p>Email: {login ? email : null}</p>
-          <p>Organization Name: {organizationName}</p>
-        </div>
-      </section>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-    </>
-  );
-};
+  // Query the database to get the organization name based on userId
+  const sql = 'SELECT o.organisationname FROM users u JOIN organizations o ON u.organisation_id = o.organisation_id WHERE u.id = ?';
 
-export default Profile;
+  db.query(sql, [userId], (err, result) => {
+    if (err) {
+      console.error('Error fetching organization data:', err);
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+
+    if (result.length > 0) {
+      const organizationname = result[0].organisationname;
+      return res.status(200).json({ success: true, organizationname });
+    } else {
+      return res.status(404).json({ success: false, error: 'Organization not found' });
+    }
+  });
+});
+
+// Update the '/add-site' endpoint to use JWT for authentication
+app.post('/add-site', authenticateToken, (req, res) => {
+  const userId = req.decoded.userId;
+
+  const siteName = req.body.site_name;
+  const siteLocation = req.body.site_location;
+
+  // Fetch the organizationId based on userId
+  const getOrganizationIdSql = 'SELECT organisation_id FROM users WHERE id = ?';
+  db.query(getOrganizationIdSql, [userId], (orgErr, orgResult) => {
+    if (orgErr) {
+      console.error('Error fetching organization ID:', orgErr);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    if (orgResult.length === 0) {
+      return res.status(404).json({ error: 'Organization not found' });
+    }
+
+    const organizationId = orgResult[0].organisation_id;
+    const siteId = uuidv4();
+
+    // Insert the new site into the sites table
+    const insertSiteSql = 'INSERT INTO sites (site_id, organisation_id, site_name, site_location) VALUES (?, ?, ?, ?)';
+    db.query(insertSiteSql, [siteId, organizationId, siteName, siteLocation], (siteErr, siteResult) => {
+      if (siteErr) {
+        console.error('Error adding site:', siteErr);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      return res.status(200).json({ success: true, siteId });
+    });
+  });
+});
+
+// Update the '/sites/:organizationId' endpoint to use JWT for authentication
+app.get('/sites', authenticateToken, (req, res) => {
+  const userId = req.decoded.userId;
+
+  // Fetch the organizationId based on userId
+  const getOrganizationIdSql = 'SELECT organisation_id FROM users WHERE id = ?';
+  db.query(getOrganizationIdSql, [userId], (orgErr, orgResult) => {
+    if (orgErr) {
+      console.error('Error fetching organization ID:', orgErr);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    if (orgResult.length === 0) {
+      return res.status(404).json({ error: 'Organization not found' });
+    }
+
+    const organizationId = orgResult[0].organisation_id;
+
+    // Replace this with your database query to fetch sites based on organization ID
+    const getSitesSql = 'SELECT * FROM sites WHERE organisation_id = ?';
+    db.query(getSitesSql, [organizationId], (err, result) => {
+      if (err) {
+        console.error('Error fetching sites:', err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      return res.status(200).json({ sites: result });
+    });
+  });
+});
+
+// Update the '/sensors/:siteId' endpoint to use JWT for authentication
+app.get('/sensors/:siteId', authenticateToken, (req, res) => {
+  const siteId = req.params.siteId;
+
+  // Replace this with your database query to fetch sensor names based on site ID
+  const getSensorNamesSql = 'SELECT sensorname FROM master WHERE site_id = ?';
+  db.query(getSensorNamesSql, [siteId], (err, result) => {
+    if (err) {
+      console.error('Error fetching sensor names:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    const sensorNames = result.map((row) => row.sensorname);
+    return res.status(200).json({ sensorNames });
+  });
+});
+
+// Update the '/sensor-data/:sensorName/:siteId' endpoint to use JWT for authentication
+app.get('/sensor-data/:sensorName/:siteId', authenticateToken, (req, res) => {
+  const sensorName = req.params.sensorName;
+  const siteId = req.params.siteId;
+
+  // Fetch sensor_id from the master table based on sensorname and site_id
+  const getSensorIdSql = 'SELECT sensorid FROM master WHERE sensorname = ? AND site_id = ?';
+  db.query(getSensorIdSql, [sensorName, siteId], (err, result) => {
+    if (err) {
+      console.error('Error fetching sensor_id:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    if (result.length === 0) {
+      return res.status(404).json({ error: 'Sensor not found' });
+    }
+
+    const sensorId = result[0].sensorid;
+
+    // Fetch data from the transactions table based on the obtained sensor_id
+    const getSensorDataSql = 'SELECT date, time, reading FROM transactions WHERE sensor_id = ?';
+    db.query(getSensorDataSql, [sensorId], (err, result) => {
+      if (err) {
+        console.error('Error fetching sensor data:', err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      const sensorData = result.map((row) => ({
+        date: row.date,
+        time: row.time,
+        reading: row.reading,
+      }));
+
+      return res.status(200).json({ sensorData });
+    });
+  });
+});
+
+// Middleware to authenticate requests using JWT
+// Middleware to authenticate requests using JWT
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization'];
+
+  console.log('Received Token:', token); // Log the received token
+
+  if (!token) {
+    console.error('No Token Provided');
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, '0GJsp4gtlpqiSccUvZCh1XB1XtAOAJTITKj0BRtlQP8', (err, decoded) => {
+    if (err) {
+      console.error('JWT Verification Error:', err.message);
+    console.log('Decoded Information:', decoded);
+    return res.sendStatus(403);
+     
+    }
+
+    console.log('Decoded Token:', decoded); // Log the decoded token
+
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
+
+app.get('/user', authenticateToken, (req, res) => {
+  // The user information is available in req.decoded
+  res.json({ success: true, user: req.decoded });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});

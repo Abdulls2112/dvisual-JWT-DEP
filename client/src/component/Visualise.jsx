92e90ee656446a7a74ac996c207fa3547c3a9670
Abdulls2112 <152040360+Abdulls2112@users.noreply.github.com>
2023-12-14 12:17:54 -0800

    Update Visualise.jsx

--- client/src/component/Visualise.jsx
+++ client/src/component/Visualise.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import {
   ResponsiveContainer,
   LineChart,
@@ -21,6 +23,7 @@ const Visualize = ({ siteId }) => {
   const [sensorData, setSensorData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   useEffect(() => {
     const fetchSensorNames = async () => {
@@ -42,7 +45,12 @@ const Visualize = ({ siteId }) => {
         if (selectedSensor) {
           setLoading(true);
           const response = await axios.get(
-            `https://dvisual-server-api.vercel.app/sensor-data/${selectedSensor}/${siteId}`
+            `https://dvisual-server-api.vercel.app/sensor-data/${selectedSensor}/${siteId}`,
+            {
+              params: {
+                date: selectedDate.toISOString().split('T')[0],
+              },
+            }
           );
           setSensorData(response.data.sensorData);
         }
@@ -55,9 +63,8 @@ const Visualize = ({ siteId }) => {
     };
 
     fetchSensorData();
-  }, [selectedSensor, siteId]);
+  }, [selectedSensor, siteId, selectedDate]);
 
-  // Function to group data by date for PieChart
   const groupDataByDate = () => {
     const groupedData = sensorData.reduce((acc, entry) => {
       const date = entry.date;
@@ -70,7 +77,6 @@ const Visualize = ({ siteId }) => {
     return Object.entries(groupedData).map(([date, reading]) => ({ date, reading }));
   };
 
-  // Function to format time for BarChart
   const formatTime = (time) => {
     const [hours, minutes, seconds] = time.split(':');
     return `${hours}:${minutes}`;
@@ -92,11 +98,19 @@ const Visualize = ({ siteId }) => {
           </option>
         ))}
       </select>
+
+      {/* Date Picker */}
+      <div style={{ margin: '20px 0', color: '#555' }}>
+        <label>Select Date:</label>
+        <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} />
+      </div>
+
       {loading && <p>Loading...</p>}
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <p style={{ color: '#555' }}>Visualization for {selectedSensor}</p>
+
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', width: '100%' }}>
-        {/* Line Chart */}
+       {/* Line Chart */}
         <div style={{ width: '30%', marginBottom: '20px', borderRadius: '10px', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)', backgroundColor: 'white', padding: '20px' }}>
           <h4 style={{ color: '#555' }}>Line Chart for {selectedSensor}</h4>
           <ResponsiveContainer width="100%" height={400}>
